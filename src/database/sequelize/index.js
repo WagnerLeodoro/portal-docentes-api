951798c5ded69a27bd7a63fed0fc4bb927751ee0
WagnerLeodoro <wagner.augustoleodoro@gmail.com>
2023-11-09 23:57:36 -0300

    config connection

--- src/database/sequelize/index.js
+++ src/database/sequelize/index.js
@@ -14,13 +14,13 @@ const conn =
   process.env.NODE_ENV !== 'production'
     ? new Sequelize(dataBaseConfig)
     : new Sequelize(
+        process.env.DATABASE,
         process.env.DATABASE_USER,
         process.env.DATABASE_PASSWORD,
-        process.env.DATABASE,
         {
+          host: process.env.DATABASE_HOST,
           dialect: 'postgres',
           port: process.env.DATABASE_PORT,
-          host: process.env.DATABASE_HOST,
           logging: true,
           native: false,
         },
